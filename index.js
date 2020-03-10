a92b701bbafa49b1b61e4fc9dba6052a73412220
moataz osama <moataz.osama@hotmail.com>
2020-03-10 07:17:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "HEllo, Neil deGrasse Tyson"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
